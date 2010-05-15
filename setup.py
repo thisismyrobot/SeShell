10ffe383ec2f425919828c84061fb90adf94eaf1
Robert Wallhead <rwallhead@gmail.com>
2010-05-15 20:44:23 +1000

    Added basic distutils setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(name='Mapper',
+      version='0.1',
+      py_modules=['mapper'],
+      package_data={'': ['README.txt']},
+      author='Robert Wallhead',
+      author_email='rwallhead@gmail.com',
+      maintainer='Robert Wallhead',
+      maintainer_email='rwallhead@gmail.com',
+      long_description='Tool to map strings to python methods',
+     )
