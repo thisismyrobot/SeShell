551558d24375ca133bca576096334baf046a0cbd
Robert Wallhead <rwallhead@gmail.com>
2010-06-10 21:04:48 +1000

    Added GNU GPL (3) notices

--- seshell.py
+++ seshell.py
@@ -1,4 +1,30 @@
-""" SeShell
+"""
+
+SeShell - Daemon
+================
+
+Copyright 2010 Robert Wallhead.
+
+SeShell is distributed under the terms of the GNU General Public License.
+
+GNU General Public License
+--------------------------
+
+This file is part of SeShell.
+
+SeShell is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SeShell is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SeShell.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import libxml2
 import re
